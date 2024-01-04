ab85b27b30c8d4181c6f89fa2e3c4a2cb8e788e1
IchiroTsune <tsunejii16@gmail.com>
2024-01-04 18:26:08 +0900

    create-react-app実施

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react_ssr
 React.js_基礎4_SSRをやってみよう
+
+1/4
+create-react-app でアプリケーションを作成
+gh-pages
---
+++ self_profile/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React.js 基礎4 SSRをやってみよう</h1>
+      </header>
+      <main>
+        <h2>自己紹介</h2>
+        <ul>
+          <li>名前：津根一朗</li>
+          <li>職業：会社員</li>
+          <li>年齢：２６</li>
+          <li>性別：男</li>
+          <li>趣味：ジョギング</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
